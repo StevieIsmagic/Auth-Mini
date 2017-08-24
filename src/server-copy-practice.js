072389dc7a6b5c2808d9335fdeb9d49d144722a2
Colombianito66 <colombianito66@gmail.com>
2017-08-24 13:39:48 -0400

    finish rewriting server for practice

--- src/server-copy-practice.js
+++ src/server-copy-practice.js
@@ -52,49 +52,48 @@ server.get('/accepted-answer/:soID', findPostMiddleWare, (req,res) => {
 	});
 });
 
-server.get('/top-answer')
+server.get('/top-answer/:soID', findPostMiddleWare, (req, res) => {
+	const post = req.post;
+	const query = Post.findOne({
+		soID: { $ne: post.acceptedAnswerID },
+		parentID: post.soID,
+  })
+  .sort({ score: 'desc' });
 
+  queryAndThen(query, res, (answer) => {
+  	if (!answer) {
+  		sendUserError('No top answer', res);
+  	} else {
+  		res.json(answer);
+  	}
+  });
+});
 
+server.get('/popular-jquery-questions', (req, res) => {
+	const query = Post.find({
+		parentID; null,
+		tags: 'jquery',
+		$or: [
+      { score: { $gt: 5000 } },
+      { 'user.reputation': { $gt: 200000 } }
+		]
+	});
 
+	queryAndThen(query, res, posts => res.json(posts));
+});
 
+server.get('/npm-answers', (req, res) => {
+	const query = Post.find({
+		parentID: null,
+		tags: 'npm'
+	});
 
+	queryAndThen(query, res, (posts) => {
+		const answerQuery = Post.find({
+			parentID: { $in: posts.map(p => p.soID) }
+		});
+		queryAndThen(answerQuery, res, answers => res.json(answers));
+	});
+})	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = { server };
